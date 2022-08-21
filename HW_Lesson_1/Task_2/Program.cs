51893abf4800f38ef9531067e420bcf2e73ad097
Abdukhamid <a.davurov@gmail.com>
2022-08-21 17:52:23 +0600

    Completed Homework #2

--- HW_Lesson_1/Task_2/Program.cs
+++ HW_Lesson_1/Task_2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите номер недели:");
+int num = int.Parse(Console.ReadLine());
+if (num < 0 | num > 7)
+{
+    Console.WriteLine("Вы ввели не существуюущий номер недели!");
+}
+else
+{
+    if (num == 1) Console.WriteLine("Понедельник");
+    if (num == 2) Console.WriteLine("Вторник");
+    if (num == 3) Console.WriteLine("Среда");
+    if (num == 4) Console.WriteLine("Четверг");
+    if (num == 5) Console.WriteLine("Пятница");
+    if (num == 6) Console.WriteLine("Суббота");
+    if (num == 7) Console.WriteLine("Воскресенье");
+}
