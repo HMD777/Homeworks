25927c39f2ee0de80f2e6d44ec8a0a8e0c2f0f92
Abdukhamid <a.davurov@gmail.com>
2022-08-21 18:37:42 +0600

    Completed Homework #5

---
+++ HW_Lesson_1/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Показать четные числа от 1 до N");
+Console.WriteLine("Введите любое число от 1 и больше:");
+int num = int.Parse(Console.ReadLine());
+
+if (num >1)
+    {
+        int count = 1;
+        while (count<=num)
+            {
+                if (count%2==0)
+                {
+                    Console.WriteLine("Число '"+count+"' является четным.");
+                } 
+            count++;
+            }
+    }
+else
+    {
+        Console.WriteLine("Вы ввели не правильные данные!");
+    }
