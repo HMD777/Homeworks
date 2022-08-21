f2c97282e54b5c333c0c65a19e05829be4020063
Abdukhamid <a.davurov@gmail.com>
2022-08-21 18:12:53 +0600

    Copleted Homework #4

---
+++ HW_Lesson_1/Task_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Выяснить является ли число чётным");
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine());
+if (num%2==0)
+{
+    Console.WriteLine("Введенное число '" + num + "' является четным!");
+}
+else
+{
+     Console.WriteLine("Введенное число '" + num + "' неявляется четным!");
+}
