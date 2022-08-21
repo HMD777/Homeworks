03d816d70616b2933f80d24ec382c1c38dc5d898
Abdukhamid <a.davurov@gmail.com>
2022-08-21 17:29:15 +0600

    Completed HomeWork #1

--- HW_Lesson_1/Task_1/Program.cs
+++ HW_Lesson_1/Task_1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 * num1 == num2)
+    {
+        Console.WriteLine("Квадрат первого числа равен второму числу");
+    }
+else 
+    {
+        Console.WriteLine("Квадрат первого числа не равен второму числу");
+    }
