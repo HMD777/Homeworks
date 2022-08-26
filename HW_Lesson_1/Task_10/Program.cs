6664704795a6f2bf061017f3ddaa1f3df1f52508
Abdukhamid <a.davurov@gmail.com>
2022-08-26 21:43:07 +0600

    Completed Homework #10

---
+++ HW_Lesson_1/Task_10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Найти третью цифру числа или сообщить, что её нет");
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num>99)
+{
+    int div = 1;
+    if (num >999) div = 10;
+    if (num >9999) div = 100;
+    if (num >99999) div = 1000;
+    Console.WriteLine("Третья цифра введенного числа является: " + (num/div)%10);
+    if (num >999999) Console.WriteLine("Вы ввели слишком большое число!");
+}
+else
+{
+    Console.WriteLine("В введенном числе, отсутствует третья цифра!");
+}
