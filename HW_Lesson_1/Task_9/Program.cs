50354fa8b62f6d48acf51b7a8a5f5d2a67eed49e
Abdukhamid <a.davurov@gmail.com>
2022-08-26 21:23:04 +0600

    Completed Homework #9

---
+++ HW_Lesson_1/Task_9/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Выяснить, кратно ли число заданному, если нет, вывести остаток.");
+Console.WriteLine("Введите первое число (числитель): ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число (знаменатель): ");
+int num2 = int.Parse(Console.ReadLine());
+if (num%num2 == 0)
+{
+    Console.WriteLine("Первое число кратно второму числу!");
+}
+else
+{
+    Console.WriteLine("Первое число некратно второму числу, остаток равен: " + (num%num2));
+}
