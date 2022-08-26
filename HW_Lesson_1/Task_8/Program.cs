f18329c45d579303a946bdd017a9fbc93f278301
Abdukhamid <a.davurov@gmail.com>
2022-08-26 20:48:40 +0600

    Completed Homework #8

---
+++ HW_Lesson_1/Task_8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Удалить вторую цифру трёхзначного числа!");
+Console.WriteLine("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine("Результат 1: "+(num/100)%10 + "_" +(num%10));
+}
+else
+{
+    Console.WriteLine("Вы ввели не верные данные!");
+}
