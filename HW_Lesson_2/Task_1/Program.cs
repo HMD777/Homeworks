86bf8d3d110f56263676c86a1f6c7a3d7c6422a2
Abdukhamid <a.davurov@gmail.com>
2022-08-29 20:50:07 +0600

    Completed Homework #1 Lesson 2

---
+++ HW_Lesson_2/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Дано число обозначающее день недели. Выяснить является номер дня недели выходным");
+Console.WriteLine("Введите число дня недели: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 0 && num <8)
+{
+    if (num == 6 || num == 7)
+    {
+        Console.WriteLine($"Введенное чтсло '{num}' является выходным!");
+    }
+    else
+    {
+        Console.WriteLine($"Введенное чтсло '{num}' не является выходным!");
+    }
+}
+else
+{
+    Console.WriteLine($"Введенное чтсло '{num}' не является днем недели!");
+}
