31f2c9b17ed1bd418cb598ace78d303cd7b08e42
Abdukhamid <a.davurov@gmail.com>
2022-08-29 21:09:58 +0600

    Completed Homework #2 Lesson #2

---
+++ HW_Lesson_2/Task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("По двум заданным числам проверять является ли одно квадратом другого");
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1;
+int min = num2;
+if (num2>num1) 
+{
+    max = num2;
+    min = num1;
+}
+if (Math.Pow(min, 2) == max)
+    {
+        Console.WriteLine($"Число '{max}' являестя квадратом числа '{min}'!");
+    }
+else
+    {
+        Console.WriteLine($"Число '{max}' не являестя квадратом числа '{min}'!");
+    }
