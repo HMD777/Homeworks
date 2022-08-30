809a98a567964ee68bbdbad9e43d3ae6553ea6b1
Abdukhamid <a.davurov@gmail.com>
2022-08-30 11:20:13 +0600

    Completed Homework #4 Lesson #3

---
+++ HW_Lesson_3/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Написать программу вычисления произведения чисел от 1 до N");
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sumfunc (int a)
+{
+    int result = a;
+    for (int i = 1; i < a; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+int result = sumfunc(num);
+Console.WriteLine($"Произведение чисел от 1 до '{num}' равен '{result}'!");
