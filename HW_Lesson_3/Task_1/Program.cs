c33d6fc19cab25c533c0bf3411580781f0a2cade
Abdukhamid <a.davurov@gmail.com>
2022-08-30 10:01:08 +0600

    Completed Homework #1 Lesson #3

---
+++ HW_Lesson_3/Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Найти кубы чисел от 1 до N.");
+Console.WriteLine("Введите число: ");
+double num = double.Parse(Console.ReadLine());
+double i = 1;
+double result = 0;
+while (i<=num)
+{
+    result = Math.Pow(i, 3);
+    Console.WriteLine($"{i}^3 = {result};");
+    i++;
+}
