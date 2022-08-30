df6ffe43a8c22ed4c660d2209a62845748e310a0
Abdukhamid <a.davurov@gmail.com>
2022-08-30 12:47:04 +0600

    Completed Homewrok #5 Lesson #3

---
+++ HW_Lesson_3/Task_5/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Показать кубы чисел, заканчивающихся на четную цифру");
+Console.WriteLine("Введите длинну массива: ");
+int arrLength = int.Parse(Console.ReadLine()); 
+int[] array = new int[arrLength];
+int min = 0;
+int max = 10;
+Random random = new Random();
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = random.Next(min, max);
+}
+var str = string.Join(" ", array);
+Console.WriteLine($"Данные массива '{str}'");
+
+for (int i=0; i<=array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        double result = Math.Pow(array[i],3) % 2;
+        if (result == 0) 
+        Console.WriteLine($"Куб числа '{array[i]}' равен '{Math.Pow(array[i],3)}' и заканчивается на четную цифру!");
+    }
+}
