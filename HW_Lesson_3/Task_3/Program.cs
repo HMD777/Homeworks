6569b5db35e4179a5b8076069bc7dd644c00af5e
Abdukhamid <a.davurov@gmail.com>
2022-08-30 11:00:06 +0600

    Completed Homework #3 Lesson #3

---
+++ HW_Lesson_3/Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Подсчитать сумму цифр в числе");
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int countDigit (int a)
+{
+    int i = 0;
+    int digit = a;
+    int result = 0;
+    while (digit > 0)
+    {
+        result = result + digit % 10;
+        digit = (digit/10);
+        i++;
+    }
+    return result;
+}
+
+int result = countDigit(num);
+Console.WriteLine($"Количество цифр в числе: {result}");
