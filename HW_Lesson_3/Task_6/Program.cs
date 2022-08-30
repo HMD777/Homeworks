770cd3da9ffb544aa9fbcbfc6dba2a2c3e2363c8
Abdukhamid <a.davurov@gmail.com>
2022-08-30 13:00:13 +0600

    Completed HomeWork #6 Lesson #3

---
+++ HW_Lesson_3/Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран");
+int [] array = new int[8];
+Random random = new Random();
+int max = 10;
+int min = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(min, max);
+}
+var str = string.Join(" ", array);
+Console.WriteLine($"Дано массив: '{str}'");
+
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i] == 0 || array[i] == 1)
+    {
+        Console.WriteLine($"Индекс массива '{i}' является '{array[i]}'!");
+    }
+}
