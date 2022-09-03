6ca63c64620f69b8ef76d3a62b6852f920f9fdc0
Abdukhamid <a.davurov@gmail.com>
2022-09-03 15:50:32 +0600

    Completed Homework #2 Lesson #5

---
+++ HW_Lesson_5/Task_2/Program.cs
@@ -0,0 +1,64 @@
+﻿Console.WriteLine("Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.");
+
+Console.WriteLine("Введите длинну массива: ");
+int num = int.Parse(Console.ReadLine());
+
+int minNum = 100;
+int maxNum = 1000;
+
+int [] array = new int [num];
+
+int [] arrayGen (int [] array, int min, int max)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array [i] = random.Next(min, max);
+    }
+    return array;
+}
+
+int countOdd (int [] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] % 2 != 0) count++;
+    }
+    return count;
+}
+
+int [] fillArrayOdd (int [] array, int [] arrayOdd)
+{
+    int j = 0;
+    for (int i =0; i < array.Length; i++)
+    {
+        
+        if (array [i] % 2 != 0)
+        {
+            arrayOdd[j] = array [i];
+            j++;
+        }
+    }
+    return arrayOdd;
+}
+
+int arraySum (int [] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array [i];
+    }
+    return result;
+}
+int [] newArray = arrayGen(array, minNum, maxNum);
+int arrayOddLength = countOdd(newArray);
+int [] arrayOdd = new int [arrayOddLength];
+int [] newArrayOdd = fillArrayOdd(newArray, arrayOdd);
+var arrayView = string.Join(", ", newArray);
+var arrayViewOdd = string.Join(", ", newArrayOdd);
+int sumResult = arraySum(newArrayOdd);
+Console.WriteLine($"Дано массив из следующих чисел: {arrayView}.");
+Console.WriteLine($"Из них нечетными числами являются: {arrayViewOdd}.");
+Console.WriteLine($"Сумма нечетных чисел равна: {sumResult}.");
