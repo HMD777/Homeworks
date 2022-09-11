cb79c2ccf6e3522d6df48a89a018e2552dfe9b8a
Abdukhamid <a.davurov@gmail.com>
2022-09-11 22:27:33 +0600

    Completed Homework #2 Lesson #6

---
+++ HW_Lesson_6/Task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Задать двумерный массив следующим правилом: Aₘₙ = m+n");
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+
+void fillArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array [i, j] = (i+1)+(j+1);
+        }
+    }
+}
+
+void printArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+int [,] matrix = new int[rows, columns];
+fillArray(matrix);
+printArray(matrix);
