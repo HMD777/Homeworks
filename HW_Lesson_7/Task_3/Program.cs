867d0165ecd57b3cd81b45b266fb8a0182481951
Abdukhamid <a.davurov@gmail.com>
2022-09-17 00:17:46 +0600

    Completed Homeworks for Lesson #7

---
+++ HW_Lesson_7/Task_3/Program.cs
@@ -0,0 +1,71 @@
+﻿Console.WriteLine("Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента");
+Console.WriteLine("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество страниц в массиве: ");
+int pages = int.Parse(Console.ReadLine());
+
+void FillArray(int[,,] array, int min, int max)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(min, max);
+                for (int l = i; l >0; l--)
+                {
+                    for (int m = j; m >0; m--)
+                    {
+                        for (int n = k-1; n > 0; n--)
+                        {
+                            int num = array [i,j,k];
+                            if (i == l && j == m && k == m) break;
+                            while (num == array[l, m, n])
+                            {
+                                num = new Random().Next(min, max);
+                            }
+                            array [i,j,k] = num;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.WriteLine($"Индекс массива ({i},{j},{k}) = {array[i, j, k]}");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+if (rows > 0 && columns > 0 && pages > 0)
+{
+    Console.WriteLine("Введите минимальное число в массиве: ");
+    int min = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите максимальное число в массиве: ");
+    int max = int.Parse(Console.ReadLine());
+    int[,,] matrix = new int[rows, columns, pages];
+    FillArray(matrix, min, max);
+    PrintArray(matrix);
+}
+else
+{
+    Console.WriteLine("Ощибка! Количество строк, столбцов или страниц не могут быть отрицательными!");
+}
---
+++ HW_Lesson_7/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника");
+Console.Write("Введите количество строк: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < num; i++)
+{
+    int count = 1;
+    for (int j = 0; j < num - i; j++)
+    {
+        Console.Write("  ");
+    }
+
+    for (int k = 0; k <= i; k++)
+    {
+        Console.Write("  {0:D} ", count);
+        count = count * (i - k) / (k + 1);
+    }
+    Console.WriteLine();
+}
