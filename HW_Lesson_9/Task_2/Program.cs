0aaf5826f36388702f6857b63a6614c30cfa30d1
Abdukhamid <a.davurov@gmail.com>
2022-09-18 12:32:05 +0600

    Copied from Homework #2 Lesson #8

---
+++ HW_Lesson_9/Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Задайте значения M и N. "
++ "Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N");
+Console.WriteLine("Введите стартовое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+void SumNumbers(int start, int end)
+{
+    int result = 0;
+    if (start > end)
+    {
+        for (int i = end; i <= start; i++)
+        {
+            result = result + i;
+        }
+    }
+    else if (start < end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            result = result + i;
+        }
+    }
+    else result = start;
+    Console.WriteLine($"Сумма элекментов от '{start}' до '{end}' равен '{result}'.");
+}
+
+SumNumbers(num1, num2);
