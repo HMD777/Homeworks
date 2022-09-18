88afb772486babe79e2a2b3eee974ae53bcd1ba1
Abdukhamid <a.davurov@gmail.com>
2022-09-18 10:50:27 +0600

    Completed Homework #2 Lesson #8

---
+++ HW_Lesson_8/Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Найти сумму элементов от M до N, N и M заданы");
+Console.WriteLine("Введите стартовое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+void SumNumbers(int start, int end)
+{
+    int result = 0;
+    if (start > end)
+    {
+        for (int i = end; i <= start; i++)
+        {
+            result = result + i;
+        }
+    }
+    else if (start < end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            result = result + i;
+        }
+    }
+    else result = start;
+    Console.WriteLine($"Сумма элекментов от '{start}' до '{end}' равен '{result}'.");
+}
+
+SumNumbers(num1, num2);
