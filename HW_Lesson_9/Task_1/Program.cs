dd8a6bd215a0932986c19bf302bbe7292537adb5
Abdukhamid <a.davurov@gmail.com>
2022-09-18 12:28:39 +0600

    Copied from HomeWork #1 Lesson #8

---
+++ HW_Lesson_9/Task_1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Задайте значения M и N." +
+" Напишите программу, которая выведет все натуральные числа в промежутке от M до N");
+Console.WriteLine("Введите стартовое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+void ShowNumbers(int start, int end)
+{
+    if (start > end)
+    {
+        for (int i = start; i >= end; i--)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    else if (start < end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    else Console.WriteLine(start);
+}
+
+ShowNumbers(num1, num2);
