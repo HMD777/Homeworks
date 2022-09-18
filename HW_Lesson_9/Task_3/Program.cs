3a7394b6a0274fdbf9a3420ce9bf55a9b4046c3e
Abdukhamid <a.davurov@gmail.com>
2022-09-18 12:35:41 +0600

    Coppied from Homework #4 Lesson #8

---
+++ HW_Lesson_9/Task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Напишите программу вычисления функции Аккермана с помощью рекурсии. " +
+"Даны два неотрицательных числа m и n.");
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int AkkermanFunction(int i, int j)
+{
+    if (i == 0)
+        return j + 1;
+    else
+      if ((i != 0) && (j == 0))
+        return AkkermanFunction(i - 1, 1);
+    else
+        return AkkermanFunction(i - 1, AkkermanFunction(i, j - 1));
+}
+
+if (num1 >= 0 && num2 >= 0)
+{
+    int result = AkkermanFunction(num1, num2);
+    Console.WriteLine($"Результат вычисления функции Аккермана ввденными данными ({num1}, {num2}) является '{result}'.");
+}
+else Console.WriteLine("Вычисления функции Аккермана не допустимо с отрицательными числами");
