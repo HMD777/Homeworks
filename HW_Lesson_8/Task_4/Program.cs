8443853076b8ed7620f82e0ad21995f457b0363f
Abdukhamid <a.davurov@gmail.com>
2022-09-18 11:32:41 +0600

    Completed Homework #4 Lesson # 8

---
+++ HW_Lesson_8/Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Написать программу вычисления функции Аккермана.");
+Console.WriteLine("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+int AkkermanFunction(int i, int j)
+{
+    if (i == 0)
+        return j + 1;
+    else
+      if ((i != 0) && (j == 0))
+        return AkkermanFunction(i - 1, 1);
+    else
+        return AkkermanFunction(i - 1,AkkermanFunction(i, j - 1));
+}
+
+if (num1 >= 0 && num2 >= 0)
+{
+    int result = AkkermanFunction(num1, num2);
+    Console.WriteLine($"Результат вычисления функции Аккермана ввденными данными ({num1}, {num2}') является '{result}'.");
+}
+else Console.WriteLine("Вычисления функции Аккермана не допустимо с отрицательными числами");
