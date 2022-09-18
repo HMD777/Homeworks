131112e95ddbd401bd470aef4f2c839b3e3ae7e4
Abdukhamid <a.davurov@gmail.com>
2022-09-18 11:17:20 +0600

    Completed Homework #3 Lesson #8

---
+++ HW_Lesson_8/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+Console.WriteLine("Найти сумму цифр числа.");
+Console.WriteLine("Введите число: ");
+string num = Console.ReadLine();
+
+void NumSum(string array)
+{
+    int check = int.Parse(array);
+    int start = 1;
+    string negative = "-";
+    if (check > 0) 
+    {
+        start = 0;
+        negative = "";
+    }
+    int result = 0;
+    for (int i = start; i < array.Length; i++)
+    {
+        result += (int)Char.GetNumericValue(array[i]);
+    }
+    Console.WriteLine($"Сумма цифр числа '{array}' равен '{negative}{result}'");
+
+}
+
+NumSum(num);
