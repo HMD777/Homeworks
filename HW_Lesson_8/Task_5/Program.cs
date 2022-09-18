5a7b6e743b6793ffde34558d4c26c995d685c055
Abdukhamid <a.davurov@gmail.com>
2022-09-18 11:53:21 +0600

    Completed Homework #5 Lesson #8

---
+++ HW_Lesson_8/Task_5/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Написать программу возведения числа А в целую стень B");
+Console.WriteLine("Введите число которое необходимо вовзести в степень: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Введение степень к предыдущему числу: ");
+int sqrt = int.Parse(Console.ReadLine());
+
+void NumSqrt(int num, int sqrt)
+{
+    double result = 1;
+    if (sqrt > 0)
+    {
+        for (int i = 0; i < sqrt; i++)
+        {
+            result = result * num;
+        }
+        Console.WriteLine($"Результат высичления {num}^{sqrt} = {result}.");
+    }
+    else
+    {
+        for (int i = 0; i < -sqrt; i++)
+        {
+            result = result * num;
+        }
+        Console.WriteLine($"Результат высичления {num}^{sqrt} = {1/result}.");
+    }
+
+
+}
+
+NumSqrt(num, sqrt);
