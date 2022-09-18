b385eb552921d7365917ad300d00c78010c1bb6b
Abdukhamid <a.davurov@gmail.com>
2022-09-18 10:20:28 +0600

    Completed Homework #1 Lesson #8

---
+++ HW_Lesson_8/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Показать натуральные числа от M до N, N и M заданы.");
+Console.WriteLine("Введите стартовое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите конечное число: ");
+int num2 = int.Parse(Console.ReadLine());
+
+void ShowNumbers(int start, int end)
+{
+    if (start > end)
+    {
+        for (int i = start; i >= end; i--)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    else if (start < end)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write($"{i}, ");
+        }
+    }
+    else Console.WriteLine(start);
+}
+
+ShowNumbers(num1, num2);
