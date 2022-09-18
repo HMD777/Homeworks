e9d1c1b5496898fe315213a2f7c0eb5197d494e9
Abdukhamid <a.davurov@gmail.com>
2022-09-18 12:18:16 +0600

    Completed Homework #6 Lesson #8

---
+++ HW_Lesson_8/Task_6/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Написать программу показывающие первые N чисел, для которых"
++ "каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности"
++ "задаются пользователем");
+Console.WriteLine("Введите длинну вычисления последовательности: ");
+int count = int.Parse(Console.ReadLine());
+
+void FibonaciUserDefined(int count, int num1, int num2)
+{
+    int temp1 = num1;
+    int temp2 = num2;
+    Console.Write($"{num1}, {num2}");
+    for (int i = 0; i < count; i++)
+    {
+        int result = temp1 + temp2;
+        Console.Write($", {result}");
+        temp1 = temp2;
+        temp2 = result;
+    }
+}
+
+if (count>0)
+{
+Console.WriteLine("Введите первое число для вычисления последовательности: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число для вычисления последовательности: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.WriteLine($"Результат выичсления: ");
+FibonaciUserDefined(count, num1, num2);
+}
+else Console.WriteLine("Длинна вычисления последовательности не может быть отрицательной!");
